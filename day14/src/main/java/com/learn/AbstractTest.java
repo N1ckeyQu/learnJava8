fb9ae618eff7cc8f57664eccff3fb90cf081afaf
n1ckeyhouse <N1ckeyQu@163.com>
2021-10-20 18:12:45 +0800

    学习接口

--- day14/src/main/java/com/learn/AbstractTest.java
+++ day14/src/main/java/com/learn/AbstractTest.java
@@ -1,7 +1,27 @@
 package com.learn;
 
 public class AbstractTest {
+    public static void main(String[] args) {
+        Person p = new Person() {
+            @Override
+            public void sum() {
+                System.out.println("lala");
+            }
+        };
 
+        Fly f = new Fly() {
+            @Override
+            public void fly() {
+                System.out.println("飞翔");
+            }
+        };
+
+        f.fly();
+    }
+}
+
+interface Fly {
+    void fly();
 }
 
 abstract class Person {
